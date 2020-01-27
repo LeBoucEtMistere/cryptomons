06523c7c545e6d0a945b01436d44185e32b339de
Depasse Arthur <arthur.depasse96@gmail.com>
2020-01-27 23:50:25 +0000

    hatching basics

--- contracts/Cryptomon.sol
+++ contracts/Cryptomon.sol
@@ -6,6 +6,20 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "./Market.sol";
 
 contract Cryptomon is ERC721Full, Ownable {
+    struct BreedData {
+        uint256 with;
+        uint256 startTime;
+        uint256 endTime;
+        bool isSet;
+    }
+
+    event Breeding(uint256 parent1, uint256 parent2, uint256 endTime);
+    event Interrupted(uint256 parent1, uint256 parent2, address by);
+    event Hatched(uint256 tokenId, uint256 parent1, uint256 parent2);
+
+    uint256[] private breedingTokens;
+    mapping(uint256 => BreedData) private breeders;
+
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
@@ -40,5 +54,109 @@ contract Cryptomon is ERC721Full, Ownable {
         }
 
         return newItemId;
+    }
+
+    function getBreedingTokens() public view returns (uint256[] memory) {
+        return breedingTokens;
+    }
+
+    function hasHatched(uint256 cm1, uint256 cm2) public view returns (bool) {
+        require(
+            ownerOf(cm1) == msg.sender,
+            "Can only hatch cryptomons you own"
+        );
+        require(
+            ownerOf(cm2) == msg.sender,
+            "Can only hatch cryptomons you own"
+        );
+        require(breeders[cm1].isSet, "This cryptomon did not breed");
+        require(breeders[cm2].isSet, "This cryptomon did not breed");
+        require(breeders[cm1].with == cm2, "These 2 are not breeding together");
+        require(breeders[cm2].with == cm1, "These 2 are not breeding together");
+        return now > breeders[cm1].endTime;
+    }
+
+    function isBreeding(uint256 tokenId) public view returns (bool) {
+        return breeders[tokenId].isSet;
+    }
+
+    function breed(uint256 cm1, uint256 cm2) public {
+        require(
+            ownerOf(cm1) == msg.sender,
+            "Can only breed cryptomons you own"
+        );
+        require(
+            ownerOf(cm2) == msg.sender,
+            "Can only breed cryptomons you own"
+        );
+
+        require(!breeders[cm1].isSet, "One cryptomon is already breeding");
+        require(!breeders[cm2].isSet, "One cryptomon is already breeding");
+
+        breedingTokens.push(cm1);
+        breedingTokens.push(cm2);
+        uint256 end = now + 1 minutes + 30;
+        breeders[cm1] = BreedData(cm2, now, end, true);
+        breeders[cm2] = BreedData(cm1, now, end, true);
+
+        emit Breeding(cm1, cm2, end);
+    }
+
+    function interruptBreeding(uint256 tokenId) public {
+        require(breeders[tokenId].isSet, "This token is not breeding");
+        require(
+            !hasHatched(tokenId, breeders[tokenId].with),
+            "This token has already finished breeding"
+        );
+
+        emit Interrupted(tokenId, breeders[tokenId].with, msg.sender);
+
+        _clearBreeding(tokenId, breeders[tokenId].with);
+
+    }
+
+    function _deleteBreedingToken(uint256 index) internal {
+        require(index < breedingTokens.length, "Improper index for deleting");
+        breedingTokens[index] = breedingTokens[breedingTokens.length - 1];
+        delete breedingTokens[breedingTokens.length - 1];
+        breedingTokens.length--;
+    }
+
+    function _clearBreeding(uint256 cm1, uint256 cm2) internal {
+        breeders[cm1] = BreedData(0, 0, 0, false);
+        breeders[cm2] = BreedData(0, 0, 0, false);
+        for (uint256 i = 0; i < breedingTokens.length; i++) {
+            if (breedingTokens[i] == cm1) {
+                _deleteBreedingToken(i);
+            }
+        }
+        for (uint256 i = 0; i < breedingTokens.length; i++) {
+            if (breedingTokens[i] == cm2) {
+                _deleteBreedingToken(i);
+            }
+        }
+    }
+
+    function hatch(uint256 cm1, uint256 cm2) public {
+        // all important verifications are done in hasHatched to avoid redoing it
+        require(hasHatched(cm1, cm2), "This cryptomon is not hatched yet");
+
+        string memory newTokenUri;
+        uint256 random = uint256(blockhash(block.number - 1));
+        if (random % 2 == 0) {
+            newTokenUri = this.tokenURI(cm1);
+        } else {
+            newTokenUri = this.tokenURI(cm2);
+        }
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _safeMint(msg.sender, newItemId);
+        _setTokenURI(newItemId, newTokenUri);
+
+        _clearBreeding(cm1, cm2);
+
+        emit Hatched(newItemId, cm1, cm2);
+
     }
 }
--- test/cryptomons.js
+++ test/cryptomons.js
@@ -31,4 +31,47 @@ contract("Cryptomon test", async accounts => {
     expect(tx.logs[1].event).to.equal("Minted");
     expect(tx.logs[1].args._toMarket).to.equal(true);
   });
+  it("should be able to breed tokens", async () => {
+    const tx = await cryptomon.breed(1, 2, {
+      from: minter
+    });
+    expect(tx.logs[0].event).to.equal("Breeding");
+    expect(parseInt(tx.logs[0].args.parent1, 10)).to.equal(1);
+    expect(parseInt(tx.logs[0].args.parent2, 10)).to.equal(2);
+  });
+  it("should be able to fetch breeding tokens", async () => {
+    var prevBreedingTokens = await cryptomon.getBreedingTokens.call({
+      from: minter
+    });
+    prevBreedingTokens = prevBreedingTokens.map(x => parseInt(x, 10));
+    await cryptomon.breed(3, 4, {
+      from: minter
+    });
+    var BreedingTokens = await cryptomon.getBreedingTokens.call({
+      from: minter
+    });
+    BreedingTokens = BreedingTokens.map(x => parseInt(x, 10));
+    var diffTokens = [];
+    BreedingTokens.forEach(token => {
+      if (!prevBreedingTokens.includes(token)) diffTokens.push(token);
+    });
+    expect(diffTokens).to.eql([3, 4]);
+  });
+  it("should be able to know if still breeding", async () => {
+    await cryptomon.breed(5, 6, {
+      from: minter
+    });
+    const hasHatched = await cryptomon.hasHatched.call(5, 6, { from: minter });
+    expect(hasHatched).to.be.false;
+  });
+  it("should be able to interrupt a breeding", async () => {
+    await cryptomon.breed(7, 8, {
+      from: minter
+    });
+    const tx = await cryptomon.interruptBreeding(7, { from: minter });
+    expect(tx.logs[0].event).to.equal("Interrupted");
+    expect(parseInt(tx.logs[0].args.parent1, 10)).to.equal(7);
+    expect(parseInt(tx.logs[0].args.parent2, 10)).to.equal(8);
+    expect(tx.logs[0].args.by).to.equal(minter);
+  });
 });
